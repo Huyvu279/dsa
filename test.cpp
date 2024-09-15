db099668015a2a851edde2ca6ce3049bf5e0c71d
huy <mhuy.nb@gmail.com>
2024-09-15 21:11:06 +0700

    add

---
+++ test.cpp
@@ -0,0 +1,24 @@
+int main(){
+	int N;
+ 
+string a[100009];
+
+    cin >> N;
+    int n = 2;
+    a[0] = "0";
+    a[1] = "1";
+    int k = 0;
+    while (a[k].length() < N){
+        a[n++] = a[k] + "0";
+        a[n++] = a[k] + "1";
+        k++;
+    }
+    for (int i = k; i < n; i++)
+        cout << a[i] << endl;
+	
+	
+
+	
+	return 0;
+}
+
